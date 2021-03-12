0c0b382f1974b8d0b5114d70890baed51103e68f
RomanHerrera1 <rherrera@g.hmc.edu>
2021-03-12 09:06:44 -0800

    Changed starting letter of functions to lowercase

--- worldSim.py
+++ worldSim.py
@@ -18,21 +18,21 @@ class WorldSim:
 
     def __init__(self, time_to_run, historical_data_set):
         self.MAX_TIME_TO_RUN = time_to_run
-        self.auv_list = CreateAuvList() #What should I do for this?
-        self.shark_list = CreateSharkList() #What should I do for this?
+        self.auv_list = createAuvList() #What should I do for this?
+        self.shark_list = createSharkList() #What should I do for this?
         self.historical_data_set = historical_data_set
         self.auv_comm_msgs = [] #What should I do for this?
 
-    def RunMainLoop(self):
+    def runMainLoop(self):
         """
         Updates the sharks' states, updates the AUVs' states, and plots the world
         """
         for t in range(self.MAX_TIME_TO_RUN):
-            self.shark_list.UpdateSharkStates(t)
-            self.auv_comm_msgs = self.auv_list.UpdateAUVStates(t, self.shark_list, self.auv_comm_msgs)
+            self.shark_list.updateSharkStates(t)
+            self.auv_comm_msgs = self.auv_list.updateAUVStates(t, self.shark_list, self.auv_comm_msgs)
             plot_world(self.shark_list, self.auv_list)
 
-    def CreateAuvList(self, initial_states):
+    def createAuvList(self, initial_states):
         """
         Creates a list of AUVs given their initial positions
 
@@ -46,7 +46,7 @@ class WorldSim:
         
         return auv_list
         
-    def CreateSharkList(self, initial_states):
+    def createSharkList(self, initial_states):
         """
         Creates a list of Sharks given their initial positions
 
@@ -60,7 +60,7 @@ class WorldSim:
         
         return shark_list
 
-    def UpdateSharkStates(self, t):
+    def updateSharkStates(self, t):
         """
         Takes in a timestamp and uses historical shark data or a probabilistic 
         motion model to estimate shark movement for each shark.
@@ -68,13 +68,13 @@ class WorldSim:
         Changes the states of sharks in shark_list
         """
         for shark in self.shark_list:
-            if self.historical_data_set == nil:
-                shark.UpdateStateWithDataSet(t, self.historical_data_set)
+            if self.historical_data_set == None:
+                shark.updateStateWithDataSet(t, self.historical_data_set)
             else:
-                shark.UpdateStateWithSharkProbabilisticMotionModel(t)
+                shark.updateStateWithSharkProbabilisticMotionModel(t)
 
 
-    def UpdateAUVStates(self, t, shark_list, auv_comm_msgs):
+    def updateAUVStates(self, t, shark_list, auv_comm_msgs):
         """
         Updates the AUVs' states based on the timestamp, the sharks' information, and the AUV comm messages
 
@@ -82,14 +82,14 @@ class WorldSim:
         """
         new_comm_msgs = []
         for auv in self.auv_list:
-            shark_measurements = SimulateSharkMeasurements(auv.state, self.shark_list)
-            new_msg = auv.RunControlLoop(t, shark_measurements, self.auv_comm_msgs)
+            shark_measurements = simulateSharkMeasurements(auv.state, self.shark_list)
+            new_msg = auv.runControlLoop(t, shark_measurements, self.auv_comm_msgs)
             new_comm_msgs.append(new_msg)
         self.auv_comm_msgs = new_comm_msgs
         
 
 
-    def RunControlLoop(self, t, shark_measurements, auv_comm_msgs):
+    def runControlLoop(self, t, shark_measurements, auv_comm_msgs):
         """
         Determines the control signals for an AUV(s) given timestamp, shark information, and AUV comm messages
 
@@ -106,4 +106,4 @@ if __name__ == '__main__':
     MAX_TIME_TO_RUN = 30
     historical_data_set = []
     world_sim = WorldSim(MAX_TIME_TO_RUN, historical_data_set)
-    world_sim.RunMainLoop()
+    world_sim.runMainLoop()
